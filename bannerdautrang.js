a0112d1132f8d46e84cd10c348e6715f14e55b72
Hanlymin <tranthienltk@gmail.com>
2016-07-28 19:05:32 +0700

    Add files via upload

---
+++ bannerdautrang.js
@@ -0,0 +1,123 @@
+//please contact me sofadungthinh@gmail.com
+//website https://www.facebook.com/Sofagiarettphcm or http://sofagiarehcm.com
+
+(function($) {
+    $.fn.linhnguyen = function(options) {        
+        var defaults = {  
+	    	animation: 'fadeAndPop', //fade, fadeAndPop, none
+		    animationspeed: 300, //how fast animtions are
+		    closeonbackgroundclick: true, //if you click background will modal close?
+		    dismissmodalclass: 'close-linhnguyen-modal' //the class of a button or element that will close an open modal
+    	}; 
+
+        var options = $.extend({}, defaults, options); 
+	
+        return this.each(function() {
+        	var modal = $(this),
+        		topMeasure  = parseInt(modal.css('top')),
+				topOffset = modal.height() + topMeasure,
+          		locked = false,
+				modalBG = $('.linhnguyen-modal-bg');
+
+			if(modalBG.length == 0) {
+				modalBG = $('<div class="linhnguyen-modal-bg" />').insertAfter(modal);
+			}		    
+			modal.bind('linhnguyen:open', function () {
+			  modalBG.unbind('click.modalEvent');
+				$('.' + options.dismissmodalclass).unbind('click.modalEvent');
+				if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modal.css({'top': $(document).scrollTop()-topOffset, 'opacity' : 0, 'visibility' : 'visible'});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"top": $(document).scrollTop()+topMeasure + 'px',
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					}
+					if(options.animation == "fade") {
+						modal.css({'opacity' : 0, 'visibility' : 'visible', 'top': $(document).scrollTop()+topMeasure});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					} 
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'visible', 'top':$(document).scrollTop()+topMeasure});
+						modalBG.css({"display":"block"});	
+						unlockModal()				
+					}
+				}
+				modal.unbind('linhnguyen:open');
+			}); 	
+
+			modal.bind('linhnguyen:close', function () {
+			  if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"top":  $(document).scrollTop()-topOffset + 'px',
+							"opacity" : 0
+						}, options.animationspeed/2, function() {
+							modal.css({'top':topMeasure, 'opacity' : 1, 'visibility' : 'hidden'});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "fade") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"opacity" : 0
+						}, options.animationspeed, function() {
+							modal.css({'opacity' : 1, 'visibility' : 'hidden', 'top' : topMeasure});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'hidden', 'top' : topMeasure});
+						modalBG.css({'display' : 'none'});	
+					}		
+				}
+				modal.unbind('linhnguyen:close');
+			});     
+   	
+    	modal.trigger('linhnguyen:open')
+			
+			//Close Modal Listeners
+			var closeButton = $('.' + options.dismissmodalclass).bind('click.modalEvent', function () {
+			  modal.trigger('linhnguyen:close')
+			});
+			
+			if(options.closeonbackgroundclick) {
+				modalBG.css({"cursor":"pointer"})
+				modalBG.bind('click.modalEvent', function () {
+				  modal.trigger('linhnguyen:close')
+				});
+			}
+			$('body').keyup(function(e) {
+        		if(e.which===27){ modal.trigger('linhnguyen:close'); } // 27 is the keycode for the Escape key
+			});
+			
+			function unlockModal() { 
+				locked = false;
+			}
+			function lockModal() {
+				locked = true;
+			}	
+			
+        });//each call
+    }//orbit plugin call
+})(jQuery);
+        
+$(window).load(function() {
+		if(document.cookie.indexOf("adf") == -1)
+		{
+			document.cookie = "adfpopunder1=adf";
+			var divpopup = document.createElement("div");
+			$(divpopup).attr('id', 'myModal');
+			$(divpopup).attr('class', 'sofagiare-modal');
+			$( "body" ).append(divpopup);
+			$("#myModal").html("<a href='http://sofagiarehcm.com/sofa-gia-re.html' target='_blank'><img src='https://lh5.googleusercontent.com/-D3WcHeACZqU/V5nz_jNR3JI/AAAAAAAAAeo/w9AhRlhTEgYUWnk68L01ONFQY9WG3d7BACL0B/w851-h315-no/13310349_1796361567277944_6978980368427963395_n.jpg' width='500px'/><br/>Sofa khuyến mãi giá rẻ nhất</a>. Đạt ngay sản phẩm <h2><a class='close-sofagiare-modal'>X</a></h2>");
+			$('#myModal').sofagiare($('#myModal').data());
+		}
+});
